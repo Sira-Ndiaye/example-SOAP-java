44e509e0450ce7db9e3163c872df296e033e2e5c
Sira-Ndiaye <sirandiaye.pro@gmail.com>
2022-07-27 12:46:11 +0000

    1.1

--- CRUD_SOAP_SERVICE/src/db/BdAccess.java
+++ CRUD_SOAP_SERVICE/src/db/BdAccess.java
@@ -1,21 +1,23 @@
 package db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class BdAccess{
-	
+public class BdAccess {
+
 	public Connection connectToDB() {
-		//Chargement du driver
+		// Chargement du driver
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		}catch (ClassNotFoundException e) {}
-		
-		//CONNECTION A LA BASE
-		Connection connexion =null;
+		} catch (ClassNotFoundException e) {
+		}
+
+		// CONNECTION A LA BASE
+		Connection connexion = null;
 		try {
-			connexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/crudservices","userTest","Passer_123");
-		}catch(SQLException e) {
+			connexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/crudservices", "username", "password");
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return connexion;
